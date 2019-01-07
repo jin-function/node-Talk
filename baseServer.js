a7304fc4bd0ed23d7eb70d08fb4366f6f725337c
jin-function <45027934+jin-function@users.noreply.github.com>
2019-01-07 09:37:07 +0900

    DB connect
    채팅로그 추가.

--- baseServer.js
+++ baseServer.js
@@ -5,13 +5,40 @@ const socket = require( 'socket.io' );
 const express = require('express');
 const app = express();
 
+/* DB 연결 */
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    port     : 3306,
+    database : 'database'
+});
+connection.connect(); //메세지는 계속 항상 받는다.
+
+let ALL_room  = []; // 전체 채팅방
+let join_user = [];
+
+//전체 채팅방 목록
+connection.query('SELECT room_num, count(*) AS joiner, room_state, room_name FROM chat_rooms GROUP BY room_num, room_state, room_name', function(err, rows, fields) {
+    if (err){
+        console.log('Error while performing Query.', err);
+        throw err;
+    } else {
+        for(var i = 0; i < rows.length; i++){
+            ALL_room.push({"state" : rows[i].room_state, "room" : rows[i].room_name, "num" : rows[i].room_num});
+            console.log(ALL_room);
+        }
+    }
+});
+
 app.get('/', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', 'http://192.168.1.31');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.send('hello')
-})
+    res.send('hello');
+});
 
 const httpServer = http.createServer(app);
 var io = socket.listen( httpServer );
@@ -26,13 +53,26 @@ io.on('connection', (socket) => {
         console.log('user disconnected', socket.id);
     });
 
+    socket.on('chat Logs', (data) => {
+        //이전 작성된 메세지. 
+        var query = "SELECT * FROM chat_logs WHERE room = "+ data.room_num +" AND room_type = '"+ data.room_state +"' ORDER BY id DESC LIMIT 15";
+        connection.query(query , function(err, rows, fields){
+            if (err){
+                console.log('Error while performing Query.', err);
+                throw err;
+            } else {
+                io.in(socket.id).emit('chat Logs', rows);
+            }
+        });
+    });
+
     //채팅방 입장
-    socket.on('JoinRoom', (data, name) => {
-    	room = data;
+    socket.on('JoinRoom', (data) => {
+    	room = data.room_state + data.room_name;
         socket.join(room, () => {//room join 
 	        // 채팅방 입장유저 알림
-	        io.in(room).emit('JoinRoom', name);
-	    	console.log('join user' + name + 'room' + room);
+	        io.in(room).emit('JoinRoom', data.user_name);
+	    	console.log('user ' + data.user_name +' enters the '+ room +' room');
 	    });
     });
 
@@ -46,9 +86,9 @@ io.on('connection', (socket) => {
     });
 
     //전송받은 메세지 
-    socket.on('chat message', (name, msg) => {
+    socket.on('chat message', (data, msg, Old_time) => {
         //참여room 모두에게 메세지 전송
-        io.in(room).emit('chat message', name, msg);  
+        io.in(room).emit('chat message', data, msg, Old_time);  
     });
 });
 
--- chat.php
+++ chat.php
@@ -9,6 +9,11 @@
 <script type="text/javascript" src="/STATIC/Script/js/socket.io.js"></script>
 <script type="text/javascript" src="/STATIC/Script/js/perfect-scrollbar.min.js"></script>
 <style>
+	.ps-y-box { overflow: hidden; position: relative; }
+    .ps-y-box .ps__thumb-y { background-color: rgba(0, 0, 0, 0.4)!important; width: 8px!important; border-radius: 0px!important; 
+    right: 0px!important;}
+    .ps-y-box .ps .ps__rail-y { background-color: transparent!important; width: 10px!important;}
+
 	ul {list-style: none;}
 	textarea { min-height:100px; max-width: 100%; display: block;}
 	ul li img { background: url(../STATIC/img/profile.png) no-repeat -1px -2px; width: 40px; height: 40px; position: absolute; top: 0; left: 0; background-size: auto; border-radius: 50%; border: 1px solid #ddd;}
@@ -43,12 +48,9 @@
 
 
 	/*#chat-box .Cbody*/
-    #chat-box .Cbody { width: 100%; height: 420px; margin-bottom: 110px; margin-top: 70px; border-bottom: 1px solid; border-top: 1px solid; border-color:#4a4c51; position: relative;}
+    #chat-box .Cbody { width: 100%; height: 420px; margin-bottom: 110px; margin-top: 70px; border-bottom: 1px solid; border-top: 1px solid; border-color:#4a4c51; }
 
-	#chat-box .Cbody article,
-	#chat-box .Cbody ul{ height: 100%;}
-
-	#chat-box .Cbody #room-list { width:100%; position: absolute; left: 0; top: 0; transition: 0.4s; background: #363636; z-index: 9999;}
+	#chat-box .Cbody #room-list { width:100%; position: absolute; left: 0; top: 0; transition: 0.4s; background: #363636;}
     #chat-box .Cbody #room-list ul li { position: relative; padding: 10px 0px; cursor: pointer; border-top:1px solid transparent;}
     #chat-box .Cbody #room-list ul li:hover { background: #474747; border-top: 1px solid #626262;}
 	room-list { width:100%; position: absolute; left: 0; top: 0; transition: 0.4s; background: #363636; z-index: 9999;}
@@ -61,15 +63,15 @@
     #chat-box .Cbody #room-list ul li h5 span { color: #999; position: absolute; right: 10px; font-weight: 300; bottom: 0; }
     #chat-box .Cbody #room-list ul li p { position: relative; padding-top: 5px; white-space: nowrap; height: 30px; overflow: hidden; 
         text-overflow: ellipsis; padding-right: 100px;}
-    #chat-box .Cbody #room-list ul li p i { font-style: inherit; width: 20px; height: 20px; display: block; position: absolute; right: 0; top: 0; margin: 5px 15px; text-align: center; background: #ff2f2f; line-height: 19px; border-radius: 10px; font-weight: 700;}
+    #chat-box .Cbody #room-list ul li p i { font-style: inherit; width: auto; height: 20px; display: block; position: absolute; right: 0; 
+    	top: 0; margin: 5px 15px; text-align: center; background: #ff2f2f; line-height: 19px; border-radius: 10px; font-weight: 700;
+    	padding: 0px 6px 0px 7px;}
     #chat-box .Cbody #room-list ul li img { margin: 10px; }
     #chat-box .Cbody #room-list ul li span { color:orange; }
     #chat-box .Cbody #room-list ul .test-box { padding-left: 60px; }
     #chat-box .Cbody #room-list ul.active {display: block;}
 
-    #chat-box .Cbody  #messages {
-      list-style-type: none; margin: 0; padding: 10px; background: #363636;
-    }
+    #chat-box .Cbody  #messages { list-style-type: none; margin: 0; padding: 10px; background: #363636; }
     #chat-box .Cbody #messages li { position: relative; font-family: auto; font-size: 11px; }
     
     #chat-box .Cbody #messages li span {vertical-align: bottom;}
@@ -147,72 +149,276 @@ var socket = io.connect( url +':'+ port);
 
 const my_name   = prompt('What your name');
 var chat_box    = $('#chat-box');
-var msg_box		= $('#messages');
-var msg 		= ''; 
+var msg_box		= $('#messages ul');
+var msg 		= ''; 	// 송.수신 메세지
 var room_html	= ''; 
-var join_room	= [];
-var RoomData 	= [];
+var join_room	= [];	// 각 채팅방 정보
+var RoomData 	= [];	// 이미 참여한 채팅방 목록
+var room_num	= 0;
+
+var chatData 	= [];
+var New_time 	= '';	// 메세지 송신
+var Old_time 	= '';	// 메세지 수신
+
+// AM,PM Format
+function TimeFormat(){  //unix time change format
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();  
+    var hour_format = '12';
+    var ext = '';
+
+    if(hour_format == '12'){
+        if(hour > 12){            
+            ext = '오후';
+            hour = (hour - 12);
+            if(hour < 10){
+                result = "0" + hour;
+            }else if(hour == 12){
+                hour = "00";
+                ext = '오전';
+            }
+        }
+        else if(hour < 12){
+            result = ((hour < 10) ? "0" + hour : hour);
+            ext = '오전';
+        }else if(hour == 12){
+            ext = '오후';
+        }
+    }
+    if(min < 10){
+        min = "0" + min; 
+    }
+    New_time = ext + ' ' + hour + ':' + min;
+    return New_time;
+}
+TimeFormat(); 
+
+//perfect-scroll
+const ps = new PerfectScrollbar('.Cbody', {
+    wheelSpeed: 0.2,
+    wheelPropagation: true,
+    minScrollbarLength: 5
+});
+
+//scroll position last line
+function ScrollBottom(){
+    Cbody_height = Cbody.prop('scrollHeight');
+    Cbody.scrollTop(Cbody_height); 
+}
+
+// html 태그 문자열 변환
+function Convert_Html(str){
+    str = str.replace(/</g,"&lt;");
+    str = str.replace(/>/g,"&gt;");
+    str = str.replace(/\"/g,"&quot;");
+    str = str.replace(/\'/g,"&#39;");
+    str = str.replace(/\n/g,"<br />");
+    return str;
+}
+
+//포커싱 이벤트
+chat_box.find('#m').focus( function(){
+    chat_box.find('.msg-box').addClass('on');
+});
+chat_box.find('#m').focusout( function(){
+    msg = $(this).val();
+
+    if(msg.length > 0){
+        chat_box.find('.msg-box').addClass('on');
+    } else {
+        chat_box.find('.msg-box').removeClass('on');  
+    }
+});
+
+// 입력 이벤트
+chat_box.find('#m').on('keyup', function(){
+    msg = $(this).val();
+
+    if(msg.length > 0){
+        chat_box.find('.msg-box').addClass('active');
+        chat_box.find('.msg-box').find('button').attr('disabled', false);
+    } else {
+        chat_box.find('.msg-box').removeClass('active');
+        chat_box.find('.msg-box').find('button').attr('disabled', true);
+    }
+});
+
 
 /* 참여한 채팅방의 정보 */
 $.getJSON("/chat/roomlist/"+my_name+"", "", function(data) { 
 }).done(function(data) {
     for(var  i = 0; i < data.length; i++){      
         room_html = '<img> </img> <div class="test-box"><h5>'+ data[i].room_name +'</h5> <p> last msg </p> </div>';
-        chat_box.find('#room-list ul').append($('<li id="room'+ data[i].room_num +'" data-room="'+i+'">').html(room_html));
-        join_room.push({'room': data[i].room_name, 'view':false, 'unconfirmed':0});
+        chat_box.find('#room-list ul').append($('<li id="room'+i+'" data-room="'+i+'">').html(room_html));
+        join_room.push({'room': data[i].room_name, 'view':false, 'unconfirmed':0}); 
         room_html = '';
     }
-    chat_box = $('#chat-box'); //동적으로 생성된 태그를 인지하기 위해 생성후 변수에 다시 저장한다.
-    RoomData = data;
-    ChatApp();
+    chat_box = $('#chat-box');  //동적으로 생성된 태그를 인지하기 위해 생성후 변수에 다시 저장한다.
+    RoomData = data;		    //불러온 방의 정보를 기록한다.
+    ChatApp(); 					//방정보를 비동기로 불러오기 때문에 소캣 연결시점을 명시해준다.
 });
+
+
+
 
 function ChatApp(){
 	$(() => {
+		var Sfirst_turn   = true;
+        var Gfirst_turn   = true;
+
+		// 모든 메세지를 전송 받기 위해 이전 참여 채팅방 모두에 자동 참가 한다.
+        for(var i =0; i < RoomData.length; i++){
+            socket.emit('JoinRoom', RoomData[i]);
+        }
+
 	    // 기존 채팅방 선택 입장
-	    chat_box.find('li').click( function(){ 
+	    chat_box.find('li').click( function(){
+	    	chat_box.find('.Cbody').scrollTop(0); 
+
 	    	var item  = $(this);
 	    	var mode  = item.parent('ul').data('mode');
+	    	room_num = item.data('room');
 	    	chat_box.removeClass().addClass(mode);
+	    	Sfirst_turn   = true;
+        	Gfirst_turn   = true;
+            for(var i =0; i < join_room.length; i++){ //선택한 채팅방을 보고 있음을 저장한다.  
+                join_room[i].view = false;  
+            }
 
-	    	if(mode == 'join'){
-	    		room_name = item.text();
-		        socket.emit('JoinRoom', room_name, my_name);
-		      	console.log('룸 선택 입장', room_name, my_name, mode);
+	    	if(mode == 'join'){ //room join mode
+		    	join_room[room_num].view = true;
+		    	join_room[room_num].unconfirmed = 0; //메세지 확인시 초기화.
+		    	item.find('p i').remove();
+        		socket.emit('chat Logs', RoomInfo[room_num]); // 해당 채팅방 로그를 가져온다.
+		      	// console.log('룸 선택 입장', RoomData[i]);
+	    	} else if (mode == 'rooms'){
+
 	    	}
+
 	    });
 
 	    // 메세지 전송
 	    chat_box.find('.Cfooter form').submit(() => {
-	    	msg = $('#m').val();
-	        socket.emit('chat message',my_name, msg);
-	        $('#m').val('');
+	    	msg = chat_box.find('#m').val();
+	        chat_box.find('#m').val('').focus();
+	        socket.emit('chat message', RoomData[room_num], msg, New_time);
+	        msg = '';
+	        chat_box.find('.msg-box').addClass('on').removeClass('active');
+            chat_box.find('.msg-box').attr('disabled', true);
 	        return false;
 	    });
 
+	    //로그메세지 출력
+        socket.on('chat Logs', function(rows){
+            for(var i = rows.length-1; i >= 0; i--){
+                // 이전 작성 메세지(limit 15)
+                if(rows[i].name == my_name){
+                    if(Sfirst_turn === true){
+                      Old_time = rows[i].msg_time;
+                      msg_box.append($('<li class="send_msg first">').html('<span class="msg-time">' + rows[i].msg_time + '</span><p>' + rows[i].msg + '</p>'));
+                      Sfirst_turn = false;                 
+                      Gfirst_turn = true;
+                    } else {
+                        if(Old_time != rows[i].msg_time){   
+                          Old_time = rows[i].msg_time;      
+                          msg_box.append($('<li class="send_msg first">').html('<span class="msg-time">' + rows[i].msg_time + '</span><p>' + rows[i].msg + '</p>'));
+                        } else {
+                          msg_box.find('li:last').find('.msg-time').remove();
+                          msg_box.append($('<li class="send_msg">').html('<span class="msg-time">' + rows[i].msg_time + '</span><p>' + rows[i].msg + '</p>'));
+                        }
+                    }
+                } else {
+                    if(Gfirst_turn === true){
+                        Old_time = rows[i].msg_time;
+                        msg_box.append($('<li class="get_msg first">').html('<img/><div class="test-box"><h5>' + rows[i].name + '</h5><p>' + rows[i].msg + '</p><span class="msg-time">' + rows[i].msg_time + '</span></div>')); 
+                        Gfirst_turn = false;         
+                        Sfirst_turn = true;
+                    } else {
+                        if(Old_time != rows[i].msg_time){ 
+                            Old_time = rows[i].msg_time;
+                            msg_box.append($('<li class="get_msg first">').html('<img/><div class="test-box"><h5>' + rows[i].name + '</h5><p>' + rows[i].msg + '</p><span class="msg-time">' + rows[i].msg_time + '</span></div>')); 
+                        } else {
+                            msg_box.find('li:last').find('.msg-time').remove();
+                            msg_box.append($('<li class="get_msg">').html('<div class="test-box"><p>' + rows[i].msg + '</p><span class="msg-time">' + rows[i].msg_time + '</span></div>'));
+                        }
+                    }
+                }
+            }
+            ScrollBottom();
+        });
+
+
 	    //메세지 출력
-	    socket.on('chat message', (name, msg) => {
-	    	msg_box.find('ul').append($('<li class="get_msg">').html('<div class="test-box"><p>'+name+':' + msg + '</p><span class="msg-time"> </span></div>'));
+	    socket.on('chat message', (data, msg, Old_time) => {
+	    	msg = Convert_Html(msg);    
+            TimeFormat();
+            for(var i = 0; i < join_room.length; i++){
+                if(join_room[i].room == data.room_name){
+                    var room = data.room_num;
+                    if(join_room[i].view == false){ //보고있는 채팅방과 전송받은 채팅방이 다를때
+                        unconfirmed = join_room[i].unconfirmed;
+                        unconfirmed++; //미확인 메세지가 전송될때마다 확인전까지 추가해준다.
+                        join_room[i].unconfirmed = unconfirmed;
+                        chat_box.find('li#room'+i+' h5').html(data.room_name + '<span>'+Old_time+'</span>');
+                        chat_box.find('li#room'+i+' p').html(msg + '<i>'+ unconfirmed +'</i>');
+                    } else {  // 전송 받은 메세지의 채팅방이 같을떄                          
+                        if(data.user_name == my_name){
+                            if(Sfirst_turn === true){
+                              msg_box.append($('<li class="send_msg first">').html('<span class="msg-time">' + New_time + '</span><p>' + msg + '</p>'));
+                              Sfirst_turn = false;                 
+                              Gfirst_turn = true;
+                            } else {
+                                if(Old_time != New_time){          
+                                  msg_box.append($('<li class="send_msg first">').html('<span class="msg-time">' + New_time + '</span><p>' + msg + '</p>'));
+                                } else {
+                                  msg_box.find('li:last').find('.msg-time').remove();
+                                  msg_box.append($('<li class="send_msg">').html('<span class="msg-time">' + Old_time + '</span><p>' + msg + '</p>'));
+                                }
+                            }
+                        } else {
+                            if(Gfirst_turn === true){
+                                msg_box.append($('<li class="get_msg first">').html('<img/><div class="test-box"><h5>' + data.user_name + '</h5><p>' + msg + '</p><span class="msg-time">' + New_time + '</span></div>')); 
+                                Gfirst_turn = false;         
+                                Sfirst_turn = true;
+                            } else {
+                                if(Old_time != New_time){ 
+                                    msg_boxappend($('<li class="get_msg first">').html('<img/><div class="test-box"><h5>' + data.user_name + '</h5><p>' + msg + '</p><span class="msg-time">' + New_time + '</span></div>')); 
+                                } else {
+                                    msg_box.find('li:last').find('.msg-time').remove();
+                                    msg_box.append($('<li class="get_msg">').html('<div class="test-box"><p>' + msg + '</p><span class="msg-time">' + Old_time + '</span></div>'));
+                                }
+                            }
+                        }
+                        ScrollBottom();
+                    }
+                }                  
+            }
 	    });
+
 
 	    //퇴장 알림
 	    socket.on('leaveRoom', (data) => {
-	        msg_box.find('ul').append($('<li class="sys_msg">').html('<p><b>' + data + '</b> 님이 채팅방을 종료하였습니다. </p>'));
+	        msg_box.append($('<li class="sys_msg">').html('<p><b>' + data + '</b> 님이 채팅방을 종료하였습니다. </p>'));
 	    });
 
 	    //입장 알림
 	    socket.on('JoinRoom', (data) => {
-	        msg_box.find('ul').append($('<li class="sys_msg">').html('<p><b>' + data + '</b> 님이 채팅방에 접속하였습니다.</p>'));        
+	        msg_box.append($('<li class="sys_msg">').html('<p><b>' + data + '</b> 님이 채팅방에 접속하였습니다.</p>'));        
 	    }); 
 
 	    //완전 퇴장 알림
 	    socket.on('deleteRoom', (data) => {
-	        msg_box.find('ul').append($('<li class="sys_msg">').html('<p><b>' + data + '</b> 님이 채팅방을 나갔습니다. </p>'));
+	        msg_box.append($('<li class="sys_msg">').html('<p><b>' + data + '</b> 님이 채팅방을 나갔습니다. </p>'));
 	    });
 
 	    //새로 입장 알림
 	    socket.on('createRoom', (data) => {
-	        msg_box.find('ul').append($('<li class="sys_msg">').html('<p><b>' + data + '</b> 님이 채팅방에 참여하였습니다.</p>'));        
+	        msg_box.append($('<li class="sys_msg">').html('<p><b>' + data + '</b> 님이 채팅방에 참여하였습니다.</p>'));        
 	    });          
 	});
 }
